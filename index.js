2eba50deac93f266eb0df9b96b4383b3d8784523
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-02-15 14:43:28 +0300

    add the correct code syntax and comments to explain about the question

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+// When comparing objects using === or ==, it checks object references, not structures.
+
+// The conditions (data === {age: 18}) and (data == {age: 18}) both evaluate to false.
+
+// Therefore, the code falls into the else block, resulting in the output "Hmm, You don't have an age."
+
+// Example:
+
 function checkAge(data) {
     if (data === {age: 18}) {
         console.log('You are an adult!');
@@ -16,3 +24,19 @@ checkAge({age: 18});
 // B: You are still an adult.
 // C: Hmm, You don't have an age
 
+// Answer: C: Hmm, You don't have an age
+
+
+// Correct  way to do this is to compare the properties of the objects.
+
+// Example:
+
+function checkAge(data) {
+    if (data.age === 18) {
+        console.log('You are an adult!');
+    } else {
+        console.log(`Hmm, You don't have an age`);
+    }
+}
+
+checkAge({age: 18});

6adb222c30ead5ec791e0a7f5a6d1ae15ea49e50
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-02-17 17:10:51 +0300

    how to use includes() method in JavaScript

--- feb-16-24/quiz1/index.js
+++ feb-16-24/quiz1/index.js
@@ -6,3 +6,12 @@ console.log(greetign);
 // How to check device memory?
 const memory = navigator.deviceMemory;
 console.log(`${memory} GB of memory`);
+
+// What will be the output of the following JavaScript code?
+console.log(1 + +"2" +"2"); // 122
+
+// check if an element is present in an array using tge includes() method
+
+const numbers = [1, 3, 13, 5, 2, 6]
+console.log(numbers.includes(5));
+console.log(numbers.includes(7));
